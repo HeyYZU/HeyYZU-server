ed6e64bc5757c086825c1626753d04ec691c5b18
Erickson Juang <a820224@gmail.com>
2017-03-28 07:18:18 +0800

    Imeplement user-proxy

---
+++ proxy/user.js
@@ -0,0 +1,163 @@
+'use strict'
+
+const request = require('request-promise')
+
+const response = (options) => {
+  return request(options).then((res) => {
+    if (res.statusCode !== 200) {
+      throw new Error({
+        body: res.body,
+        status: res.status.code
+      })
+    }
+    return res.body
+  })
+}
+
+const homeworks = (token, year, semester) => {
+  if (!token || !year || !semester) {
+    throw new Error('toke, year and semester must be given.')
+  }
+  return response({
+    method: 'POST',
+    uri: 'https://unipop.yzu.edu.tw/YzuPortalAPI/api/Portal/UserHomeWorkList',
+    form: {
+      'token': token,
+      'year': year,
+      'smtr': semester
+    },
+    resolveWithFullResponse: true,
+    json: true
+  })
+}
+
+homeworks.archive = (token, id) => {
+  if (!token || !id) {
+    throw new Error('toke and id must be given.')
+  }
+
+  return response({
+    method: 'POST',
+    uri: 'https://unipop.yzu.edu.tw/YzuPortalAPI/api/Portal/UserFileDownLoad',
+    form: {
+      'token': token,
+      'fileType': 'homework',
+      'fileID': id
+    },
+    resolveWithFullResponse: true
+  })
+}
+
+homeworks.attachment = (token, id) => {
+  if (!token || !id) {
+    throw new Error('toke and id must be given.')
+  }
+
+  return response({
+    method: 'POST',
+    uri: 'https://unipop.yzu.edu.tw/YzuPortalAPI/api/Portal/UserFileDownLoad',
+    form: {
+      'token': token,
+      'fileType': 'schoolwork',
+      'fileID': id
+    },
+    resolveWithFullResponse: true
+  })
+}
+
+const materials = (token, year, semester) => {
+  if (!token || !year || !semester) {
+    throw new Error('token, year and semester must be given.')
+  }
+
+  return response({
+    method: 'POST',
+    uri: 'https://unipop.yzu.edu.tw/YzuPortalAPI/api/Portal/UserCourseMaterials',
+    form: {
+      'token': token,
+      'year': year,
+      'smtr': semester
+    },
+    resolveWithFullResponse: true,
+    json: true
+  })
+}
+
+const announcements = (token, year, semester) => {
+  if (!token || !year || !semester) {
+    throw new Error('token, year and semester must be given.')
+  }
+
+  return response({
+    method: 'POST',
+    uri: 'https://unipop.yzu.edu.tw/YzuPortalAPI/api/Portal/UserCourseNews',
+    form: {
+      'token': token,
+      'year': year,
+      'smtr': semester
+    },
+    resolveWithFullResponse: true,
+    json: true
+  })
+}
+
+const libraryReserving = (token) => {
+  if (!token) {
+    throw new Error('token must be given.')
+  }
+
+  return response({
+    method: 'POST',
+    uri: 'https://unipop.yzu.edu.tw/YzuPortalAPI/api/Portal/UserCurrentRequest',
+    form: {
+      'token': token
+    },
+    resolveWithFullResponse: true,
+    json: true
+  })
+}
+
+const libraryReading = (token) => {
+  if (!token) {
+    throw new Error('token must be given.')
+  }
+
+  return response({
+    method: 'POST',
+    uri: 'https://unipop.yzu.edu.tw/YzuPortalAPI/api/Portal/UserCurrentLoan',
+    form: {
+      'token': token
+    },
+    resolveWithFullResponse: true,
+    json: true
+  })
+}
+
+const libraryRead = (token) => {
+  if (!token) {
+    throw new Error('token must be given.')
+  }
+
+  return response({
+    method: 'POST',
+    uri: 'https://unipop.yzu.edu.tw/YzuPortalAPI/api/Portal/UserHistoryLoan',
+    form: {
+      'token': token
+    },
+    resolveWithFullResponse: true,
+    json: true
+  })
+}
+
+module.exports = {
+  course: {
+    homeworks: homeworks,
+    materials: materials,
+    announcements: announcements
+  },
+  library: {
+    reserving: libraryReserving,
+    reading: libraryReading,
+    read: libraryRead
+  }
+}
