818f968c11fb866cd5b09bf246ce1cb31849bbef
Erickson Juang <a820224@gmail.com>
2017-03-29 06:02:02 +0800

    Add test case proxy/course

---
+++ test/proxy/course.js
@@ -0,0 +1,47 @@
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const expect = chai.expect
+const auth = require(BASE_DIR + 'proxy/auth')
+const course = require(BASE_DIR + 'proxy/course')
+
+chai.use(chaiAsPromised)
+chai.should()
+
+module.exports = (username, password, year, semester, courseId, courseClass) => {
+  describe('user proxy testing', function() {
+    this.timeout(10 * 1000)
+    // get testing token
+    let testToken = ''
+    const getToken = auth(username, password)
+      .then((r) => {
+        testToken = r.token
+      })
+    expect(getToken).to.eventually.be.fulfilled.then(() => {
+      describe('Course proxy testing', function() {
+        describe('get materials', function() {
+          it('is successful', (done) => {
+            course.materials(testToken, year, semester, courseId, courseClass)
+              .then((result) => {
+                expect(result).to.be.an('array')
+                done()
+              }, (err) => {
+                done(err)
+              })
+          })
+        })
+
+        describe('get announcements', function() {
+          it('is successful', (done) => {
+            course.announcements(testToken, year, semester, courseId, courseClass)
+              .then((result) => {
+                expect(result).to.be.an('array')
+                done()
+              }, (err) => {
+                done(err)
+              })
+          })
+        })
+      })
+    })
+  })
+}
--- test/proxy/index.js
+++ test/proxy/index.js
@@ -1,9 +1,11 @@
 const authTesting = require('./auth')
 const userTesting = require('./user')
+const courseTesting = require('./course')
 
 module.exports = (username, password, year, semester, courseId, courseClass) => {
   describe('Proxy Test', function() {
     authTesting(username, password)
     userTesting(username, password, year, semester)
+    courseTesting(username, password, year, semester, courseId, courseClass)
   })
 }
