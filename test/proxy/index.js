55d2457b4615a688454c6029e8916d31027209bb
Erickson Juang <a820224@gmail.com>
2017-04-12 22:06:09 +0800

    Enable test case - library proxy

--- test/proxy/index.js
+++ test/proxy/index.js
@@ -1,9 +1,11 @@
 const authTesting = require('./auth')
 const userTesting = require('./user')
 const courseTesting = require('./course')
+const libraryTesting = require('./library')
 
 module.exports = (username, password, year, semester, courseId, courseClass) => {
   authTesting(username, password)
   userTesting(username, password, year, semester)
   courseTesting(username, password, year, semester, courseId, courseClass)
+  libraryTesting('多益')
 }
