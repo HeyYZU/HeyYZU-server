d9090d70e24f69b937796bd52eeee1b8463d414d
Erickson Juang <a820224@gmail.com>
2017-04-12 21:58:53 +0800

    Remove comment

--- middleware/authority.js
+++ middleware/authority.js
@@ -3,7 +3,6 @@
 const userProxy = require(path.join(BASE_DIR, 'proxy/user'))
 
 module.exports = (req, res, next) => {
-  console.log(req.originalUrl)
   new Promise((resolve, reject) => {
     if (req.query.access_token === undefined) return reject(new Error('token undefined'))
     return resolve()
