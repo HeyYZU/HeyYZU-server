32e9ecc8635be87343e08d79c7d290df6479456e
Erickson Juang <a820224@gmail.com>
2017-04-12 22:02:50 +0800

    Add library transaction query & bookinfo api

--- controller/library.js
+++ controller/library.js
@@ -1,16 +1,94 @@
 'use strict'
 
-const mock = require('./mock.json').library
+const libraryProxy = require(path.join(BASE_DIR, '/proxy/library'))
+const userProxy = require(path.join(BASE_DIR, '/proxy/user'))
 const readingList = (req, res, next) => {
-  res.status(200).json(mock.reading)
+  let token = req.query.access_token
+  userProxy.library.reading(token)
+    .then((res) => {
+      // Following key name of el is from yzu api response
+      // In order to offset time zone to UTC, - 28800 for parse result of time
+      return res.map((el) => omitEmpty({
+        id: parseInt(el.Bibliosno, 10),
+        title: el.bktitle,
+        author: el.author,
+        attr: {
+          dueDate: Math.round(Date.parse(el.ReadDueDate) / 1000) - 28800,
+          fine: el.Fine > 0 ? el.Fine : null,
+          renewable: el.MaxRenewTimes > 0,
+          reserved: Math.round(Date.parse(el.RecallDate) / 1000) - 2880 <= 0
+        }
+      }))
+    })
+    .then((content) => {
+      res.status(200).json(content)
+    })
+    .catch((e) => {
+      res.status(502).json({message: 'Bad Gateway'})
+    })
 }
 
 const readList = (req, res, next) => {
-  res.status(200).json(mock.read)
+  let token = req.query.access_token
+  userProxy.library.read(token)
+    .then((res) => {
+      // Following key name of el is from yzu api response
+      // In order to offset time zone to UTC, - 28800 for parse result of time
+      return res.map((el) => ({
+        id: parseInt(el.Bibliosno, 10),
+        title: el.bktitle,
+        author: el.author
+      }))
+    })
+    .then((content) => {
+      return Promise.all(
+          content.map(el => libraryProxy.status(el.id))
+        )
+        .then((res) => {
+          return content.map(el => {
+            let book = res.find(book => parseInt(book.info.bibliosno, 10) === el.id)
+            return {
+              id: el.id,
+              title: el.title,
+              author: el.author,
+              attr: {
+                index: book.info.callno,
+                reserved: book.collections.reduce((pv, el) => pv + el.RequestCount, 0)
+              }
+            }
+          })
+        })
+    })
+    .then((content) => {
+      res.status(200).json(content)
+    })
+    .catch((e) => {
+      res.status(502).json({message: 'Bad Gateway'})
+    })
 }
 
 const reservingList = (req, res, next) => {
-  res.status(200).json(mock.reserving)
+  let token = req.query.access_token
+  userProxy.library.reserving(token)
+    .then((res) => {
+      // Following key name of el is from yzu api response
+      // In order to offset time zone to UTC, - 28800 for parse result of time
+      return res.map((el) => omitEmpty({
+        id: el.bibliosno,
+        title: el.bktitle,
+        author: el.author,
+        attr: {
+          order: el.OrderSNo,
+          reservedBefore: el.OrderSNo === 1 ? Math.round(Date.parse(el.HoldDeadLine) / 1000) - 2880 : null
+        }
+      }))
+    })
+    .then((content) => {
+      res.status(200).json(content)
+    })
+    .catch((e) => {
+      res.status(502).json({message: 'Bad Gateway'})
+    })
 }
 
 const favoriteList = (req, res, next) => {
@@ -30,7 +108,35 @@ const dashboard = (req, res, next) => {
 }
 
 const bookInfo = (req, res, next) => {
-  res.status(200).json(mock.book)
+  let bookId = req.params.id
+  libraryProxy.status(bookId)
+      // Following key name of el is from yzu api response
+    .then((res) => omitEmpty({
+      title: res.info.bktitle,
+      author: res.info.author,
+      index: res.info.callno,
+      publisher: res.info.Publisher,
+      year: res.info.publish_YY,
+      isbn: parseInt(res.info.ISBN.replace(/[^0-9]/ig, ''), 10) || null,
+      cover: res.info.Cover,
+      media: res.info.material_type === '視聽多媒體' || null,
+      ebook: res.info.material_type === '電子書' || null,
+      collections: res.collections.map((el) => ({
+        branch: el.SublibraryC || null,
+        collection: el.CollectionC || null,
+        reservingCount: el.RequestCount || null,
+        return: Math.round(Date.parse(el.RealDueDate) / 1000) - 2880 || null,
+        attr: {
+          type: el.MaterialType || null
+        }
+      }))
+    }))
+    .then((content) => {
+      res.status(200).json(content)
+    })
+    .catch((e) => {
+      res.status(502).json({message: 'Bad Gateway'})
+    })
 }
 
 module.exports = {
---
+++ proxy/library.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const request = require('request-promise')
+const logger = log4js.getLogger('proxy')
+
+const response = (options) => {
+  return request(options).then((res) => {
+    return res.body
+  }).catch((e) => {
+    logger.error('[library]' + e)
+    return Promise.reject(e)
+  })
+}
+
+const searchByTitle = (keyword) => {
+  return response({
+    uri: 'https://unipop.yzu.edu.tw/OpenAPI/api/lib/keyword/title=' + encodeURIComponent(keyword),
+    resolveWithFullResponse: true,
+    json: true
+  })
+}
+
+const searchByISBN = (ISBN) => {
+  return response({
+    uri: 'https://unipop.yzu.edu.tw/OpenAPI/api/lib/keyword/isbn=' + ISBN,
+    resolveWithFullResponse: true,
+    json: true
+  })
+}
+
+const bookStatus = (id) => {
+  const createPromise = (key, requestOptions) => {
+    return response(requestOptions)
+      .then((res) => ({key: key, res}))
+      .catch((err) => {
+        logger.info('[bookStatus]Retry - ' + requestOptions.uri)
+        return createPromise(key, requestOptions)
+      })
+  }
+
+  return Promise.all([
+    createPromise('info', {
+      uri: 'https://unipop.yzu.edu.tw/OpenAPI/api/lib/keyword/sys=' + id,
+      resolveWithFullResponse: true,
+      json: true
+    }),
+    createPromise('collections', {
+      uri: 'https://unipop.yzu.edu.tw/OpenAPI/api/lib/Holding/' + id,
+      resolveWithFullResponse: true,
+      json: true
+    })
+  ]).then((res) => res.reduce((pv, el) => {
+    pv[el.key] = el.key === 'info' ? el.res[0] : el.res
+    return pv
+  }, {}))
+  .catch((e) => {
+    return Promise.reject(e)
+  })
+}
+
+module.exports = {
+  search: {
+    title: searchByTitle,
+    ISBN: searchByISBN
+  },
+  status: bookStatus
+}
