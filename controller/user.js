2d83067749fffb8640a073a1e8d9b97bc2cf93ed
Erickson Juang <a820224@gmail.com>
2017-04-12 21:58:21 +0800

    Implement user's curriculum

--- controller/user.js
+++ controller/user.js
@@ -1,8 +1,42 @@
 'use strict'
 
-const mock = require('./mock.json').user
+const util = require(path.join(BASE_DIR, '/util'))
+const userProxy = require(path.join(BASE_DIR + '/proxy/user'))
 const curriculum = (req, res, next) => {
-  res.status(200).json(mock.curriculum)
+  let query = req.query
+
+  userProxy.course.curriculum(query.access_token, util.time.academicYear(), util.time.semester())
+    .then((res) => {
+      // Following key name of el is from yzu api response
+      return res.map((el) =>
+        ({
+          lesson_id: (
+            el.year.replace(/[^0-9]/ig, '') +
+            el.smtr.replace(/[^0-9]/ig, '') + '_' +
+            el.cos_id.replace(/[^0-9A-Z]/ig, '') + '_' +
+            el.cos_class.replace(/[^0-9A-Z]/ig, '')
+            ),
+          name: el.name,
+          timeSlots: el.WeekandRoom.split(',')
+                      .filter(el => el.length > 0)
+                      .map((el) => {
+                        let pattern = el.match(/(\d{1})(\d{2})\(([^ ].+)?\)/)
+                        return ({
+                          day: parseInt(pattern[1], 10),
+                          slot: parseInt(pattern[2], 10),
+                          room: pattern[3]
+                        })
+                      })
+        }))
+    })
+    .then((content) => {
+      res.status(200).json(content)
+    })
+    .catch((e) => {
+      res.status(502).json({
+        message: 'Bad Gateway'
+      })
+    })
 }
 
 const info = (req, res, next) => {
